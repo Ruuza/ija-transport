4be77b66cde3032455305d03620bd9bb7cc5bac4
side14 <stargrasss@gmail.com>
2020-05-07 22:30:20 +0200

    Basic GUI functionality

---
+++ xgolda02/src/ija/ija2020/guiMaps/GuiStreet.java
@@ -0,0 +1,38 @@
+package ija.ija2020.guiMaps;
+
+import ija.ija2020.main.MapObject;
+import ija.ija2020.maps.Coordinate;
+import ija.ija2020.maps.Stop;
+import ija.ija2020.maps.Street;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.Shape;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GuiStreet extends Street implements MapObject {
+
+    public GuiStreet(String Id, Coordinate... points) {
+        super(Id, points);
+    }
+
+    @Override
+    public List<Shape> getEl() {
+        List<Shape> lines = new ArrayList<Shape>();
+        Coordinate last = null;
+        for(Coordinate c: this.getCoordinates()){
+            if(last!=null){
+                lines.add(new Line(last.getX(), last.getY(), c.getX(), c.getY()));
+            }
+            last = c;
+        }
+
+        for(Stop s: this.getStops()){
+            GuiStop gS = new GuiStop(s);
+            for(Shape shape: gS.getEl()){
+                lines.add(shape);
+            }
+        }
+        return lines;
+    }
+}
