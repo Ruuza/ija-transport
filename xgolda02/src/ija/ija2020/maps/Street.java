0cf1183851ee35dbb09ebfe651f5ffc69d636937
Ruuza <RunyCZ@gmail.com>
2020-05-07 15:03:37 +0200

    update Street.java

--- xgolda02/src/ija/ija2020/maps/Street.java
+++ xgolda02/src/ija/ija2020/maps/Street.java
@@ -11,6 +11,8 @@ public class Street {
     private List<Coordinate> coordinates = new ArrayList<>();
     private String Id;
 
+    private final double SELECTION_FUZZINESS = 3;
+
     private List<Stop> Stops;
 
     public Street(String Id, Coordinate... points) {
@@ -23,22 +25,56 @@ public class Street {
 
     public boolean addStop(Stop stop) {
 
-        // this.Stops.add(stop);
-        // stop.setStreet(this);
         for (int i = 0; i < coordinates.size() - 1; i++) {
             Coordinate a = coordinates.get(i);
             Coordinate b = coordinates.get(i + 1);
 
-            // distance(A, C) + distance(B, C) == distance(A, B)
+            Coordinate leftPoint;
+            Coordinate rightPoint;
 
-            double x = a.distance(stop.getCoordinate()) + b.distance(stop.getCoordinate());
-            double y = a.distance(b);
+            // Normalize start/end to left right to make the offset calc simpler.
+            if (a.getX() <= b.getX()) {
+                leftPoint = a;
+                rightPoint = b;
+            } else {
+                leftPoint = b;
+                rightPoint = a;
+            }
 
-            System.out.println("x:" + x);
-            System.out.println("y:" + y);
+            // If point is out of bounds, no need to do further checks.
+            if (stop.getCoordinate().getX() + SELECTION_FUZZINESS < leftPoint.getX()
+                    || rightPoint.getX() < stop.getCoordinate().getX())
+                continue;
+            else if (stop.getCoordinate().getY() + SELECTION_FUZZINESS < Math.min(leftPoint.getY(), rightPoint.getY())
+                    || Math.max(leftPoint.getY(), rightPoint.getY()) < stop.getCoordinate().getY()
+                            - SELECTION_FUZZINESS)
+                continue;
 
-            if (x == y)
+            double deltaX = rightPoint.getX() - leftPoint.getX();
+            double deltaY = rightPoint.getY() - leftPoint.getY();
+
+            // If the line is straight, the earlier boundary check is enough to determine
+            // that the point is on the line.
+            // Also prevents division by zero exceptions.
+            boolean lineContains;
+            if (deltaX != 0 && deltaY != 0) {
+
+                double slope = deltaY / deltaX;
+                double offset = leftPoint.getY() - leftPoint.getX() * slope;
+                double calculatedY = stop.getCoordinate().getX() * slope + offset;
+
+                // Check calculated Y matches the points Y coord with some easing.
+                lineContains = stop.getCoordinate().getY() - SELECTION_FUZZINESS <= calculatedY
+                        && calculatedY <= stop.getCoordinate().getY() + SELECTION_FUZZINESS;
+            } else {
+                lineContains = true;
+            }
+
+            if (lineContains) {
+                this.Stops.add(stop);
+                stop.setStreet(this);
                 return true;
+            }
         }
 
         return false;
--- xgolda02/src/ija/ija2020/maps/UnitTests.java
+++ xgolda02/src/ija/ija2020/maps/UnitTests.java
@@ -3,6 +3,10 @@ package ija.ija2020.maps;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 
+/**
+ * @author Petr Ruzansky
+ * @author Koci
+ */
 public class UnitTests {
     @Test
     public void testStreet01() {
@@ -37,5 +41,124 @@ public class UnitTests {
 
         Assertions.assertFalse(s1.follows(s2), "Ulice first a second na sebe nenavazuji");
         Assertions.assertTrue(s1.follows(s3), "Ulice first a third na sebe navazuji");
+    }
+
+    @Test
+    public void testStreet02() {
+        Coordinate c1, c2, c3;
+        Street s1;
+        Stop stop1;
+
+        c1 = new Coordinate(100, 100);
+        c2 = new Coordinate(100, 200);
+        c3 = new Coordinate(150, 200);
+        s1 = new Street("first", c1, c2, c3);
+        Assertions.assertNotNull(s1, "Ulice first - lze vytvorit");
+
+        c1 = new Coordinate(170, 200);
+        stop1 = new Stop("stop1", c1);
+        Assertions.assertFalse(s1.addStop(stop1), "Zastavka je mimo ulici");
+        Assertions.assertTrue(s1.getStops().isEmpty(), "Ulice je prazdna");
+
+        c1 = new Coordinate(130, 200);
+        stop1 = new Stop("stop2", c1);
+        Assertions.assertTrue(s1.addStop(stop1), "Zastavka je na ulici");
+        Assertions.assertFalse(s1.getStops().isEmpty(), "Ulice neni prazdna");
+
+        c1 = new Coordinate(-45, -33);
+        c2 = new Coordinate(35, 42);
+        c3 = new Coordinate(-20, -10);
+        s1 = new Street("first", c1, c2);
+        stop1 = new Stop("stop3", c3);
+        Assertions.assertTrue(s1.addStop(stop1), "Zastavka je na ulici");
+        Assertions.assertFalse(s1.getStops().isEmpty(), "Ulice neni prazdna");
+
+        c1 = new Coordinate(-45, -33);
+        c2 = new Coordinate(35, 42);
+        c3 = new Coordinate(-21, -11);
+        s1 = new Street("first", c1, c2);
+        stop1 = new Stop("stop4", c3);
+        Assertions.assertTrue(s1.addStop(stop1), "Zastavka je na ulici");
+        Assertions.assertFalse(s1.getStops().isEmpty(), "Ulice neni prazdna");
+
+        c1 = new Coordinate(-45, -33);
+        c2 = new Coordinate(35, 42);
+        c3 = new Coordinate(-20, -9);
+        s1 = new Street("first", c1, c2);
+        stop1 = new Stop("stop5", c3);
+        Assertions.assertTrue(s1.addStop(stop1), "Zastavka je na ulici");
+        Assertions.assertFalse(s1.getStops().isEmpty(), "Ulice neni prazdna");
+
+        c1 = new Coordinate(-45, -33);
+        c2 = new Coordinate(35, 42);
+        c3 = new Coordinate(-20, -5);
+        s1 = new Street("first", c1, c2);
+        stop1 = new Stop("stop6", c3);
+        Assertions.assertFalse(s1.addStop(stop1), "Zastavka neni na ulici");
+        Assertions.assertTrue(s1.getStops().isEmpty(), "Ulice je prazdna");
+
+        c1 = new Coordinate(-45, -33);
+        c2 = new Coordinate(35, 42);
+        c3 = new Coordinate(-16, -5);
+        s1 = new Street("first", c1, c2);
+        stop1 = new Stop("stop5", c3);
+        Assertions.assertTrue(s1.addStop(stop1), "Zastavka je na ulici");
+        Assertions.assertFalse(s1.getStops().isEmpty(), "Ulice neni prazdna");
+
+        c1 = new Coordinate(-45, -33);
+        c2 = new Coordinate(35, 42);
+        c3 = new Coordinate(2, 11);
+        s1 = new Street("first", c1, c2);
+        stop1 = new Stop("stop5", c3);
+        Assertions.assertTrue(s1.addStop(stop1), "Zastavka je na ulici");
+        Assertions.assertFalse(s1.getStops().isEmpty(), "Ulice neni prazdna");
+
+        c1 = new Coordinate(-45, -33);
+        c2 = new Coordinate(35, 42);
+        c3 = new Coordinate(2, 12);
+        s1 = new Street("first", c1, c2);
+        stop1 = new Stop("stop5", c3);
+        Assertions.assertTrue(s1.addStop(stop1), "Zastavka je na ulici");
+        Assertions.assertFalse(s1.getStops().isEmpty(), "Ulice neni prazdna");
+
+        c1 = new Coordinate(-1985, -1703);
+        c2 = new Coordinate(1690, 1388);
+        c3 = new Coordinate(35, -4);
+        s1 = new Street("first", c1, c2);
+        stop1 = new Stop("stop5", c3);
+        Assertions.assertTrue(s1.addStop(stop1), "Zastavka je na ulici");
+        Assertions.assertFalse(s1.getStops().isEmpty(), "Ulice neni prazdna");
+
+        c1 = new Coordinate(4, 2);
+        c2 = new Coordinate(5, 6);
+        c3 = new Coordinate(5, 4);
+        s1 = new Street("first", c1, c2);
+        stop1 = new Stop("stop5", c3);
+        Assertions.assertTrue(s1.addStop(stop1), "Zastavka je na ulici");
+        Assertions.assertFalse(s1.getStops().isEmpty(), "Ulice neni prazdna");
+
+        // c1 = new Coordinate(4, 2);
+        // c2 = new Coordinate(5, 6);
+        // c3 = new Coordinate(4, 5);
+        // s1 = new Street("first", c1, c2);
+        // stop1 = new Stop("stop5", c3);
+        // Assertions.assertTrue(s1.addStop(stop1), "Zastavka je na ulici");
+        // Assertions.assertFalse(s1.getStops().isEmpty(), "Ulice neni prazdna");
+
+        c1 = new Coordinate(4, 2);
+        c2 = new Coordinate(5, 6);
+        c3 = new Coordinate(5, 5);
+        s1 = new Street("first", c1, c2);
+        stop1 = new Stop("stop5", c3);
+        Assertions.assertTrue(s1.addStop(stop1), "Zastavka je na ulici");
+        Assertions.assertFalse(s1.getStops().isEmpty(), "Ulice neni prazdna");
+
+        c1 = new Coordinate(4, 2);
+        c2 = new Coordinate(5, 6);
+        c3 = new Coordinate(4, 4);
+        s1 = new Street("first", c1, c2);
+        stop1 = new Stop("stop5", c3);
+        Assertions.assertTrue(s1.addStop(stop1), "Zastavka je na ulici");
+        Assertions.assertFalse(s1.getStops().isEmpty(), "Ulice neni prazdna");
     }
 }
